ce36da499fc904e8e2ac0d9993a5876f2ba7840b
vinoth kumar <vinothak1220@gmail.com>
2021-01-06 17:39:54 +0530

    second commit

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const app = express();
-const port = 8000
+const port = 8080
+app.get('/', (req, res) => {
+    console.log('woooow works now')
+})
 app.listen(port, () => console.log('running on port', port))
 
